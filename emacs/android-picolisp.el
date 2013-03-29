22a4468fcc672fb662e3c233ae2694690a0a3aa3
Dmitry Mozgin <m0391n@gmail.com>
2013-03-29 06:31:19 +0400

    Added upload-picolisp-to-android function

--- emacs/android-picolisp.el
+++ emacs/android-picolisp.el
@@ -3,7 +3,7 @@
   :prefix "android-picolisp")
 
 (defcustom ap-adb-directory
-  "/adb::/data/local"
+  "/adb::/data/local/tmp"
   "adb tramp path"
   :type 'directory
   :group 'android-picolisp)
@@ -14,12 +14,17 @@
   :type 'directory
   :group 'android-picolisp)
 
-;; TODO: add upload of picolisp to android
-
-;; phone
 (defun upload-picolisp-to-android()
   (interactive)
-  )
+  (let ((name "adb-upload-picolisp")
+        (native-picolisp-tar (expand-file-name "data/native-picolisp.tar"
+                                               (file-name-directory (locate-library "android-picolisp")))))
+    (when (file-exists-p native-picolisp-tar)
+      (let ((buffer (get-buffer-create (format "*%s*" name))))
+        (start-process name buffer "adb" "push" native-picolisp-tar "/data/local/tmp/np.tar")
+        (start-process name buffer "adb" "shell"
+                       "cd /data/local/tmp; rm -r pil np.tar; tar xf np.tar; rm -r np.tar;")
+        (message "Uploading picolisp - done.")))))
 
 (defun run-picolisp-adb ()
   (interactive)
